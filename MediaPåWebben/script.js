d1d5593bba843bd5eb697616a1c0569c4f1c0b32
ErikTeknikum <mr.towerik@gmail.com>
2023-12-18 20:01:25 +0100

    audio

---
+++ MediaPåWebben/script.js
@@ -0,0 +1,11 @@
+
+var audioController = document.getElementById("buttonAudio3")
+
+function playAudio() {
+    audioController.play();
+}
+
+
+function pauseAudio() {
+    audioController.pause();
+}
